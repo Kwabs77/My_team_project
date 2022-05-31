9a928136bb41a529521a7be0c8a9f32a6b31f167
Kwabena Owusu Achiaw <kwabena.owusu-achiaw@amalitech.org>
2022-05-31 11:11:01 +0000

    changes made to the CSS, HTML, and JavaScript

--- mobile.js
+++ mobile.js
@@ -17,4 +17,121 @@ close.addEventListener('click',()=>{
 })
 
 
+
+
  
+
+
+// for Meet 
+
+let details= document.querySelector('.meet-box');
+let quote = document.querySelector('.meet-quote');
+let expandClose = document.querySelector('.expand-close')
+
+details.addEventListener('click', ()=>{
+    quote.style.display='block';
+    details.style.display='none';
+
+})
+
+expandClose.addEventListener('click', ()=>{
+    quote.style.display='none';
+    details.style.display='flex';
+})
+
+// for Meet 1
+
+
+let details1= document.querySelector('.meet-box-1');
+let quote1 = document.querySelector('.meet-quote-1');
+let expandClose1 = document.querySelector('.expand-close-1')
+
+details1.addEventListener('click', ()=>{
+    quote1.style.display='block';
+    details1.style.display='none';
+
+})
+
+expandClose1.addEventListener('click', ()=>{
+    quote1.style.display='none';
+    details1.style.display='flex';
+})
+
+// for meet 2
+let details2= document.querySelector('.meet-box-2');
+let quote2 = document.querySelector('.meet-quote-2');
+let expandClose2 = document.querySelector('.expand-close-2')
+
+details2.addEventListener('click', ()=>{
+    quote2.style.display='block';
+    details2.style.display='none';
+
+})
+
+expandClose2.addEventListener('click', ()=>{
+    quote2.style.display='none';
+    details2.style.display='flex';
+})
+
+
+// for meet 3
+
+let details3= document.querySelector('.meet-box-3');
+let quote3 = document.querySelector('.meet-quote-3');
+let expandClose3 = document.querySelector('.expand-close-3')
+
+details3.addEventListener('click', ()=>{
+    quote3.style.display='block';
+    details3.style.display='none';
+
+})
+
+expandClose3.addEventListener('click', ()=>{
+    quote3.style.display='none';
+    details3.style.display='flex';
+})
+
+
+
+// for meet 4
+
+let details4= document.querySelector('.meet-box-4');
+let quote4 = document.querySelector('.meet-quote-4');
+let expandClose4 = document.querySelector('.expand-close-4')
+
+details4.addEventListener('click', ()=>{
+    quote4.style.display='block';
+    details4.style.display='none';
+
+})
+
+expandClose4.addEventListener('click', ()=>{
+    quote4.style.display='none';
+    details4.style.display='flex';
+})
+
+
+// for meet 5 
+let details5= document.querySelector('.meet-box-5');
+let quote5 = document.querySelector('.meet-quote-5');
+let expandClose5 = document.querySelector('.expand-close-5')
+
+details5.addEventListener('click', ()=>{
+    quote5.style.display='block';
+    details5.style.display='none';
+
+})
+
+expandClose5.addEventListener('click', ()=>{
+    quote5.style.display='none';
+    details5.style.display='flex';
+})
+
+
+
+
+
+
+
+
+
